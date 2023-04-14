9494271877599a7053665f799d3db6b205b2d23e
Zhengyi Lai <zheng1@kubesphere.io>
2023-04-14 11:31:59 +0800

    excel2markdown support pr link and author
    Signed-off-by: Zhengyi Lai <zheng1@kubesphere.io>

--- cmd/release-notes/excel2markdown.py
+++ cmd/release-notes/excel2markdown.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Convert an Excel file to Markdown')
 parser.add_argument('input_file', type=str, help='path to input Excel file')
@@ -10,20 +11,36 @@ df = pd.read_excel(args.input_file)
 
 result = {}
 
+def get_release_note_and_link(note):
+    # Example:
+    #   Add the Observability Center. ([kse-console#195](https://github.com/kubesphere/kse-console/pull/195), [@harrisonliu5](https://github.com/harrisonliu5))
+    pr_link = note["pr_url"]
+    match = re.match(r"https://github.com/kubesphere/(.*)/pull/(\d+)", pr_link)
+    pr_link_text = "%s#%s" % (match.group(1), match.group(2))
+    release_note = note["release_note"]
+    author = note["author"]
+    author_link = "https://github.com/%s" % author
+    return "%s ([%s](%s), [@%s](%s))" % (release_note, pr_link_text, pr_link, author, author_link)
+
 for _, row in df.iterrows():
     if isinstance(row['Release Note'], str):
         module = result.setdefault(row['Module'], {})
         type = module.setdefault(row['Type'], [])
-        type.append(row['Release Note'])
+        if isinstance(row['pr_url'], str):
+            type.append({
+                "release_note": row['Release Note'],
+                "pr_url": row['pr_url'],
+                "author": row['author'],
+            })
 
 md = ""
 for module, types in result.items():
 
-    md += "## %s \n\n" % module
+    md += "## %s\n\n" % module
     for type, notes in types.items():
-        md += "### %s \n\n" % type
+        md += "### %s\n\n" % type
         for note in notes:
-            md += "- %s \n" % note
+            md += "- %s\n" % get_release_note_and_link(note)
         md += '\n'
     md += '\n'
 
